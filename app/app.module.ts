110c78ae9fea38872d9a31429b807f01ddf0924d
sudharsanam narasimhan <sudharsanamnarasimhan@sudharsanams-MacBook-Pro.local>
2016-11-26 01:10:15 +0530

    Addressing review comments

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,7 @@
 import {NgModule}      from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent}  from './app.component';
-import {Ng2C3} from './library/ng2C3.component';
+import {Ng2C3} from './library/ng2c3.component';
 
 
 @NgModule({
