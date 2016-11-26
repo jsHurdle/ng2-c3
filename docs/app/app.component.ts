1ae6fb70bc6022367140fcc9f006b05aaf1c8c0a
Arunkumar S <asrisailapathi@vmware.com>
2016-11-26 20:20:15 +0530

    Now the setup is perfectly working for dynamic inputs. Going forward update the types

--- docs/app/app.component.ts
+++ docs/app/app.component.ts
@@ -8,14 +8,84 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
     private _data : any;
+    private _chartOptions : any;
+    private _chartConfig : any;
+
+    private _chartTypes : any;
+    private _allCharts : any;
+
+    private _c3Configs : Array<string> = ['axis', 'tooltip', 'grid', 'legend', 'zoom', 'regions', 'subchart'];
+    private _c3Options : Array<string> = ['size', 'padding', 'color', 'interaction', 'transition', 'point', 'line', 'area', 'bar', 'pie', 'donut', 'gauge'];
+
     constructor() {
-        this._data = {
-            x: 'x',
-            columns: [
-                ['x', 30, 50, 100, 230, 300, 310],
-                ['data1', 30, 200, 100, 400, 150, 250],
-                ['data2', 130, 300, 200, 300, 250, 450]
-            ]
+
+        this._allCharts = {
+            'line': {
+                data: {
+                    x: 'x',
+                    columns: [
+                        ['x', 30, 50, 100, 230, 300, 310],
+                        ['data1', 30, 200, 100, 400, 150, 250],
+                        ['data2', 130, 300, 200, 300, 250, 450]
+                    ]
+                }
+            },
+            'bar': {
+                data: {
+                    columns: [
+                        ['data1', 30, 200, 100, 400, 150, 250],
+                        ['data2', 130, 100, 140, 200, 150, 50]
+                    ],
+                    type: 'bar'
+                },
+                chartOptions: {
+                    bar: {
+                        width: {
+                            ratio: 0.5
+                        }
+                    }
+                }
+            },
+            'pie': {
+                data: {
+                    columns: [
+                        ['data1', 30],
+                        ['data2', 120],
+                    ],
+                    type : 'pie'
+                }
+            }
         };
+
+        this._chartTypes = [{
+            key: 'bar',
+            text: 'Bar Chart'
+        },
+        {
+            key: 'pie',
+            text: 'Pie Chart'
+        },
+        {
+            key: 'line',
+            text: 'Line Chart'
+        }];
+
+        this.handleChartChange(this._chartTypes[0].key);
+    }
+
+    public handleChartChange ( type: string) : void {
+        let _object : any = this._allCharts[type];
+        if(_object['data']) {
+            this._data = _object['data'];
+            for(let key in _object) {
+                if(this._c3Configs.indexOf(key) !== -1) {
+                    this._chartConfig[key] = _object[key];
+                }
+                else if (this._c3Options.indexOf(key) !== -1) {
+                    this._chartOptions[key] = _object[key];
+                }
+            }
+            
+        }
     }
 }
